b3efdba45192d537924b7b48b376badec50f70d3
Your Namesaleons <sergio-grafo@hotmail.com>
2023-04-17 22:02:42 -0500

    cord general

--- src/main/java/com/team2/team2/Team2Application.java
+++ src/main/java/com/team2/team2/Team2Application.java
@@ -17,7 +17,7 @@ public class Team2Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://tea2.herokuapp.com").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
